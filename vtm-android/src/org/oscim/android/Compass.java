41f1b359c5426905380710caca4dabeb3110a872
Mars <info@webelite.be>
2014-08-19 16:37:40 +0200

    Smoothened sensor data and fixed orientation issue
    used lowPass method found on stackoverflow to smoothen sensor data, and added orientation fix block. map should now update smoothly when using Compass

--- vtm-android/src/org/oscim/android/Compass.java
+++ vtm-android/src/org/oscim/android/Compass.java
@@ -29,33 +29,32 @@ public class Compass implements SensorEventListener {
     private final SensorManager mSensorManager;
     private final Sensor mAccelerometer;
     private final Sensor mMagnetometer;
+    private final Context ctx;
     private Map mMap = null;
     private CompassUpdateListener compassUpdateListener;
 
-    private float[] mLastAccelerometer = new float[3];
-    private float[] mLastMagnetometer = new float[3];
+    private float[] mLastAccelerometer = new float[9];
+    private float[] mLastMagnetometer = new float[9];
+
     private boolean mLastAccelerometerSet = false;
     private boolean mLastMagnetometerSet = false;
 
-    private float[] mR = new float[9];
+    private float[] mR = new float[16];
     private float[] mOrientation = new float[3];
 
-    public Compass(Context ctx, Map map){
+    public Compass(Context ctx, Map map) {
         this(ctx);
         this.mMap = map;
     }
 
     public Compass(Context ctx) {
+        this.ctx = ctx;
         // get SensorManager from Context
         mSensorManager = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
 
         // get Sensors
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-
-        // reset
-        mLastAccelerometerSet = false;
-        mLastMagnetometerSet = false;
     }
 
     /**
@@ -65,6 +64,9 @@ public class Compass implements SensorEventListener {
         // register sensors and this(listener)
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
         mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_UI);
+
+        mLastAccelerometerSet = false;
+        mLastMagnetometerSet = false;
     }
 
     /**
@@ -131,23 +133,61 @@ public class Compass implements SensorEventListener {
     //
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        // clone sensor data so it doesn't change while we are using it
+        float[] sensorValues = sensorEvent.values.clone();
+
         // check sensore type
         // store latest sensor data locally
         if (sensorEvent.sensor == mAccelerometer) {
-            System.arraycopy(sensorEvent.values, 0, mLastAccelerometer, 0, sensorEvent.values.length);
+            lowPass(sensorValues, mLastAccelerometer);
+
             mLastAccelerometerSet = true;
         } else if (sensorEvent.sensor == mMagnetometer) {
-            System.arraycopy(sensorEvent.values, 0, mLastMagnetometer, 0, sensorEvent.values.length);
+            lowPass(sensorValues, mLastMagnetometer);
+
             mLastMagnetometerSet = true;
+        } else {
+            // oops, wrong sensor
+            return;
         }
+
         // if both sensor data has been received measure orientation
         if (mLastAccelerometerSet && mLastMagnetometerSet) {
-            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            boolean success = SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
+            if (!success)
+                return; // failed to get RotationMatrix, abort mission
+
+            if (sensorEvent.sensor == mAccelerometer) {
+                // fix orientation of sensor values
+                switch (((WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation()) {
+                    case Surface.ROTATION_0:
+                        SensorManager.remapCoordinateSystem(mR, SensorManager.AXIS_X, SensorManager.AXIS_Y, mR);
+                        break;
+
+                    case Surface.ROTATION_90:
+                        SensorManager.remapCoordinateSystem(mR, SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X, mR);
+                        break;
+
+                    case Surface.ROTATION_180:
+                        SensorManager.remapCoordinateSystem(mR, SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y, mR);
+                        break;
+
+                    case Surface.ROTATION_270:
+                        SensorManager.remapCoordinateSystem(mR, SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X, mR);
+                        break;
+                }
+            }
+
+            // get final orientation values
             SensorManager.getOrientation(mR, mOrientation);
 
+            // update map if needed
             if (mMap != null) {
-                mMap.viewport().setRotation(getAngleDegrees());
-                mMap.updateMap(true);
+                float mapAngle = mMap.getMapPosition().getBearing();
+                if (Math.abs(mapAngle - getAngleDegrees()) > 0.25) {
+                    mMap.viewport().setRotation(-getAngleDegrees());
+                    mMap.updateMap(true);
+                }
             }
 
             // report compass orientation change
@@ -155,10 +195,32 @@ public class Compass implements SensorEventListener {
                 compassUpdateListener.compassUpdateReceived(mOrientation[0], mOrientation[1], mOrientation[2]);
             }
         }
+
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    /*
+     * copied from http://stackoverflow.com/questions/4611599/help-smoothing-data-from-a-sensor/5780505#5780505
+     * time smoothing constant for low-pass filter
+     * 0 ≤ α ≤ 1 ; a smaller value basically means more smoothing
+     * See: http://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
+     *
+     * @see http://en.wikipedia.org/wiki/Low-pass_filter#Algorithmic_implementation
+     * @see http://en.wikipedia.org/wiki/Low-pass_filter#Simple_infinite_impulse_response_filter
+     */
+    static final float ALPHA = 0.1f;
+
+    protected float[] lowPass(float[] input, float[] output) {
+        if (output == null) return input;
+
+        for (int i = 0; i < input.length; i++) {
+            output[i] = (input[i] * ALPHA) + (output[i] * (1.0f - ALPHA));
+        }
+        return output;
     }
 }
